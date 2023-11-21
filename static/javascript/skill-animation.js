097d537ba59eecc21bcee232551d4129f616d49f
David Chen <bdfd2005@gmail.com>
2023-11-21 16:41:38 -0500

    Update Skill Section

---
+++ static/javascript/skill-animation.js
@@ -0,0 +1,59 @@
+/**
+ * Template Name: iPortfolio - v3.3.0
+ * Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
+(function () {
+	"use strict";
+
+	/**
+	 * Easy selector helper function
+	 */
+	const select = (el, all = false) => {
+		el = el.trim();
+		if (all) {
+			return [...document.querySelectorAll(el)];
+		} else {
+			return document.querySelector(el);
+		}
+	};
+
+	/**
+	 * Easy event listener function
+	 */
+	const on = (type, el, listener, all = false) => {
+		let selectEl = select(el, all);
+		if (selectEl) {
+			if (all) {
+				selectEl.forEach((e) => e.addEventListener(type, listener));
+			} else {
+				selectEl.addEventListener(type, listener);
+			}
+		}
+	};
+
+	/**
+	 * Easy on scroll event listener
+	 */
+	const onscroll = (el, listener) => {
+		el.addEventListener("scroll", listener);
+	};
+
+	/**
+	 * Skills animation
+	 */
+	let skilsContent = select(".skills-content");
+	if (skilsContent) {
+		new Waypoint({
+			element: skilsContent,
+			offset: "80%",
+			handler: function (direction) {
+				let progress = select(".progress .progress-bar", true);
+				progress.forEach((el) => {
+					el.style.width = el.getAttribute("aria-valuenow") + "%";
+				});
+			},
+		});
+	}
+})();
